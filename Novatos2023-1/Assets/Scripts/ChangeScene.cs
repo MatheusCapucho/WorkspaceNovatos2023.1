e0a9e25bea278e6e3d890259a1cf8ebcb88ea380
MatheusCapucho <matheusfcapucho@gmail.com>
2023-05-20 15:19:54 -0300

    Aula01 - Classes

---
+++ Novatos2023-1/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+        }
+            
+    }
+
+
+}
--- Novatos2023-1/Assets/Scripts/SimpleMovement.cs
+++ Novatos2023-1/Assets/Scripts/SimpleMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,16 +7,43 @@ public class SimpleMovement : MonoBehaviour
 {
     private Vector3 _input;
     private Rigidbody2D _rigidbody;
+    private Collider2D _collider;
     [SerializeField]
     private float _velocity = 5f;
+
     void Start()
     {
-        _rigidbody = GetComponent<Rigidbody2D>();
+ 
     }
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _collider = GetComponent<Collider2D>();
+
+    }
+   
 
     void Update()
     {
         _input.x = Input.GetAxisRaw("Horizontal");
-        _rigidbody.velocity = _velocity * _input;
+        _input.y = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            ApertarBorao();
+        }
+
+
+    }
+
+    private void ApertarBorao()
+    {
+      
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.velocity = _velocity * _input; // Vector2(1f, 0f);
     }
 }
